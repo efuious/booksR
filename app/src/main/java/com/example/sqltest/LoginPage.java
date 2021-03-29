489e3de046a2628050c48bc6c17fb02ff0cab8f5
voyage1984 <voyage1984@gmail.com>
2021-03-29 14:27:53 +0800

    完善功能：注册

--- app/src/main/java/com/example/sqltest/LoginPage.java
+++ app/src/main/java/com/example/sqltest/LoginPage.java
@@ -2,6 +2,7 @@ package com.example.sqltest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 public class LoginPage extends AppCompatActivity implements View.OnClickListener {
 
     private EditText edit_username,edit_password;
-    private Button login_btn;
+    private Button login_btn,register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +27,9 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
         this.edit_username = findViewById(R.id.login_username);
         this.edit_password = findViewById(R.id.login_password);
         this.login_btn = findViewById(R.id.login_login_btn);
-
+        this.register_btn = findViewById(R.id.login_register_btn);
         this.login_btn.setOnClickListener(this);
+        this.register_btn.setOnClickListener(this);
     }
 
     @Override
@@ -43,6 +45,10 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
             else{
                 Toast.makeText(this,"登录失败！",Toast.LENGTH_SHORT).show();
             }
+        }
+        else if(view.getId() == R.id.login_register_btn){
+            Intent register = new Intent(LoginPage.this,Register.class);
+            startActivity(register);
         }
     }
 }
---
+++ app/src/main/java/com/example/sqltest/Register.java
@@ -0,0 +1,74 @@
+package com.example.sqltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+    EditText r_name,r_sex,r_birthday,r_pswd;
+    Button register_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile);
+
+        init();
+    }
+
+    public void init(){
+        r_name = findViewById(R.id.cp_name);
+        r_sex = findViewById(R.id.cp_sex);
+        r_birthday = findViewById(R.id.cp_birthday);
+        r_pswd = findViewById(R.id.cp_pswd);
+        register_btn = findViewById(R.id.cp_save);
+        register_btn.setText("注册");
+        register_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        Toast.makeText(this,"注册",Toast.LENGTH_SHORT).show();
+
+        String new_name = r_name.getText().toString();
+        String new_birthday = r_birthday.getText().toString();
+        String new_sex = r_sex.getText().toString();
+        String new_pswd = r_pswd.getText().toString();
+        int get_sex = new Whale().checkSex(new_sex);
+        if(get_sex>1){
+            Toast.makeText(this,"请输入正确的性别！",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        System.out.println(new_name + " " + new_birthday + " " + new_sex + " " + new_pswd);
+        DB_Demo db_demo = new DB_Demo(this);
+
+        if (!new_name.isEmpty()&&!new_birthday.isEmpty()&&!new_sex.isEmpty()&&!new_pswd.isEmpty()) {
+            try {
+                boolean flag = db_demo.register(new_name,get_sex,new_birthday,new_pswd);
+                if(flag==true) {
+                    System.out.println("注册成功！");
+                    Toast.makeText(this,"注册成功！",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else{
+                    System.out.println("注册失败！");
+                    Toast.makeText(this,"注册失败！请检查输入",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                Toast.makeText(this,"注册失败！请检查输入",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+    }
+
+}
