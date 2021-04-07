11ed986d5dd2b0c9337e4aab3a93ba5e9df0fd2e
voyage1984 <voyage1984@gmail.com>
2021-04-07 17:07:16 +0800

    弃用文件

--- app/src/main/java/com/example/sqltest/SettingPage.java
+++ app/src/main/java/com/example/sqltest/SettingPage.java
@@ -1,23 +1,13 @@
 package com.example.sqltest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.app.Activity;;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.sql.Connection;
 
-public class SettingPage extends AppCompatActivity {
-
-    EditText ip;
-    Button save_btn;
-
-    private MyDatabase mydb;
+/*
+ * 废弃文件， 不再使用
+ */
+public class SettingPage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
